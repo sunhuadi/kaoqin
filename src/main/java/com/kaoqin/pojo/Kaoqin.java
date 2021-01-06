492ead421b133684bca0fe6d8ec50447dfd2d1e7
刘森 <1320732874@qq.com>
2021-01-06 11:27:17 +0800

    考勤类

---
+++ src/main/java/com/kaoqin/pojo/Kaoqin.java
@@ -0,0 +1,16 @@
+package com.kaoqin.pojo;
+
+import lombok.Data;
+
+@Data
+
+public class Kaoqin {
+    int id;
+    String stuNo;
+    String stuName;
+    String stuClass;
+    String stage;
+    Data kaoqinTime;
+    int cid;
+    String teacher;
+}
