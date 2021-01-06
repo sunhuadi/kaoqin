6f28f9497e1200e2c9f758201612d6328470278e
刘森 <1320732874@qq.com>
2021-01-06 22:19:13 +0800

    dfjhdb

---
+++ src/main/java/com/kaoqin/controller/KaoqinController.java
@@ -0,0 +1,26 @@
+package com.kaoqin.controller;
+
+import com.kaoqin.mapper.KaoqinMapper;
+import com.kaoqin.mapper.CourseMapper;
+import com.kaoqin.mapper.TestMapper;
+import com.kaoqin.pojo.Course;
+import com.kaoqin.pojo.Teacher;
+import com.kaoqin.pojo.Kaoqin;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+@Controller
+public class KaoqinController {
+    @Autowired
+    KaoqinMapper kaoqinMapper;
+
+    @GetMapping("/test2")
+    @ResponseBody
+    public List<Kaoqin> check(){
+        return kaoqinMapper.qurryKaoqin("170101");
+    }
+}
---
+++ src/main/java/com/kaoqin/mapper/KaoqinMapper.java
@@ -0,0 +1,13 @@
+package com.kaoqin.mapper;
+
+import com.kaoqin.pojo.Kaoqin;
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Mapper
+@Repository
+public interface KaoqinMapper {
+    List<Kaoqin> qurryKaoqin(String stuNo);
+}
